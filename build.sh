b37618819ab85bfb6473f9d6d840be7ee7c0b4d4
Jared Hughes <jahughes241@gmail.com>
2024-06-27 02:11:05 -0700

    Remove gitignore generated by wasm-pack

--- build.sh
+++ build.sh
@@ -3,3 +3,5 @@ set -euo pipefail
 
 wasm-pack build --target web --out-dir public/pkg
 cp index.html public/
+# Can't gitignore anything you want JamesIves/github-pages-deploy-action to publish.
+rm public/pkg/.gitignore
